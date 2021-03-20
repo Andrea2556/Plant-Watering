e06bd2f2a1a1d912151f55dbffd0be6fb4f077e0
Andrea <andreavlas97@gmail.com>
2021-03-20 17:37:35 +0200

    Aici am lucrat eu. Urmeaza Simion

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-import { createPlant, getPlant } from "./localStoragePlants.js";
-console.log(createPlant);
-console.log(getPlant);
+import {
+  createPlant,
+  getPlant,
+  getHTMLPlant,
+  getAllPlants,
+  initHTMLPlants,
+  addButtonHere,
+  // loadImage,
+} from "./localStoragePlants.js";
+console.log(getPlant());
+// console.log(getHTMLPlant());
+// console.log(getAllPlants());
+console.log(createPlant());
+// console.log();
+initHTMLPlants();
+console.log(addButtonHere);
+// loadImage;
